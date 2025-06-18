953a42ffa6ce84a2a38ce6a5450afa806b6f87f6
Anmol Painuly <anmolpainuly12@gmail.com>
2025-06-18 20:32:34 +0530

    NextJS Config and ShanCN installation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier", "plugin:tailwindcss/recommended"],
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": "error",
+      "react/no-unescaped-entities": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
